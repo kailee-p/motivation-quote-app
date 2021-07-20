b588ee46821f4fada487abc42a6c2557a7cc703d
Kailee Pedersen <kaileepedersen@gmail.com>
2021-07-19 23:17:50 -0400

    UI for buttons created

--- Motivation Quote App/ViewController.swift
+++ Motivation Quote App/ViewController.swift
@@ -12,6 +12,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //set background color
+        self.view.backgroundColor = UIColor(red: 0.4, green: 0.3765, blue: 0.8745, alpha: 1.0) /* #6660df */
+        
+        //inspiration button
+        let inspirationButton = UIButton(frame: CGRect(x: 100, y: 300, width: self.view.frame.width * 0.9, height: 50))
+        inspirationButton.backgroundColor = UIColor(red: 0.4627, green: 0.8078, blue: 0.9922, alpha: 1.0) /* #76cefd */
+        inspirationButton.setTitle("Inspiration", for: .normal)
+        inspirationButton.center.x = view.center.x
+        self.view.addSubview(inspirationButton)
+        
+        //motivation button
+        let motivationButton = UIButton(frame: CGRect(x: 100, y: 360, width: self.view.frame.width * 0.9, height: 50))
+        motivationButton.backgroundColor = UIColor(red: 0.9804, green: 0.8235, blue: 0, alpha: 1.0) /* #fad200 */
+        motivationButton.setTitle("Motivation", for: .normal)
+        motivationButton.center.x = view.center.x
+        self.view.addSubview(motivationButton)
+        
+        //life button
+        let lifeButton = UIButton(frame: CGRect(x: 100, y: 420, width: self.view.frame.width * 0.9, height: 50))
+        lifeButton.backgroundColor = UIColor(red: 0.9725, green: 0.6235, blue: 0, alpha: 1.0) /* #f89f00 */
+        lifeButton.setTitle("Life", for: .normal)
+        lifeButton.center.x = view.center.x
+        self.view.addSubview(lifeButton)
+        
+        //friendship button
+        let friendshipButton = UIButton(frame: CGRect(x: 100, y: 480, width: self.view.frame.width * 0.9, height: 50))
+        friendshipButton.backgroundColor = UIColor(red: 0.3255, green: 0.8078, blue: 0.3608, alpha: 1.0) /* #53ce5c */
+        friendshipButton.setTitle("Friendship", for: .normal)
+        friendshipButton.center.x = view.center.x
+        self.view.addSubview(friendshipButton)
     }
 
 
