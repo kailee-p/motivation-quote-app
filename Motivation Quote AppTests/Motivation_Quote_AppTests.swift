c4462523370d78d35cb644f60c3cbcdf0793b76c
Kailee Pedersen <kaileepedersen@Kailees-Mac-mini.local>
2021-07-19 22:23:19 -0400

    first commit

---
+++ Motivation Quote AppTests/Motivation_Quote_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Motivation_Quote_AppTests.swift
+//  Motivation Quote AppTests
+//
+//  Created by Kailee Pedersen on 7/19/21.
+//
+
+import XCTest
+@testable import Motivation_Quote_App
+
+class Motivation_Quote_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
