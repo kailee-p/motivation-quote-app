e115d93d501163dfbb667605186d1597830999d0
Kailee Pedersen <kaileepedersen@gmail.com>
2021-07-19 23:52:51 -0400

    Pushing my first attempt at linking buttons with functions

--- Motivation Quote App/ViewController.swift
+++ Motivation Quote App/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
         inspirationButton.backgroundColor = UIColor(red: 0.4627, green: 0.8078, blue: 0.9922, alpha: 1.0) /* #76cefd */
         inspirationButton.setTitle("Inspiration", for: .normal)
         inspirationButton.center.x = view.center.x
+        inspirationButton.addTarget(self, action: #selector(inspirationButtonAction), for: UIControl.Event.touchUpInside)
         self.view.addSubview(inspirationButton)
         
         //motivation button
@@ -28,6 +29,7 @@ class ViewController: UIViewController {
         motivationButton.backgroundColor = UIColor(red: 0.9804, green: 0.8235, blue: 0, alpha: 1.0) /* #fad200 */
         motivationButton.setTitle("Motivation", for: .normal)
         motivationButton.center.x = view.center.x
+        inspirationButton.addTarget(self, action: #selector(motivationButtonAction), for: UIControl.Event.touchUpInside)
         self.view.addSubview(motivationButton)
         
         //life button
@@ -35,6 +37,7 @@ class ViewController: UIViewController {
         lifeButton.backgroundColor = UIColor(red: 0.9725, green: 0.6235, blue: 0, alpha: 1.0) /* #f89f00 */
         lifeButton.setTitle("Life", for: .normal)
         lifeButton.center.x = view.center.x
+        inspirationButton.addTarget(self, action: #selector(lifeButtonAction), for: UIControl.Event.touchUpInside)
         self.view.addSubview(lifeButton)
         
         //friendship button
@@ -42,9 +45,23 @@ class ViewController: UIViewController {
         friendshipButton.backgroundColor = UIColor(red: 0.3255, green: 0.8078, blue: 0.3608, alpha: 1.0) /* #53ce5c */
         friendshipButton.setTitle("Friendship", for: .normal)
         friendshipButton.center.x = view.center.x
+        inspirationButton.addTarget(self, action: #selector(friendshipButtonAction), for: UIControl.Event.touchUpInside)
         self.view.addSubview(friendshipButton)
     }
-
-
+    
+    @objc func inspirationButtonAction() {
+        print("Inspo button pushed")
+    }
+    
+    @objc func motivationButtonAction() {
+        print("motiv button pushed")
+    }
+    
+    @objc func lifeButtonAction() {
+        print("life button pushed")
+    }
+    
+    @objc func friendshipButtonAction () {
+        print("friendship button pushed")
+    }
 }
-
